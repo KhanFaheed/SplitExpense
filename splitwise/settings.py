e40dd34003d0a2f8986a83421437cb159917d1cd
KhanFaheed <faheedk215@gmail.com>
2025-03-25 14:44:32 +0300

    navbar logic and frontend

--- splitwise/settings.py
+++ splitwise/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL='media/'
+STATIC_URL = '/static/'
+MEDIA_URL='/media/'
 
 STATICFILES_DIRS=[
     BASE_DIR/'static',
